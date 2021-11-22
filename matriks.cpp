dcf5b105ef13536483074d1fc7c57bf6f406c239
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-22 10:58:52 +0700

    Program C++

---
+++ matriks.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int matriks [2][4] = {{1,2,3,4},{5,6,7,8}};
+	int baris, kolom;
+	for (baris=0;baris<2;baris++){
+		for (kolom=0;kolom<4;kolom++){
+			cout<<matriks[baris][kolom]<<" ";
+		}
+	cout<<endl;
+	}
+	return 0;
+}
