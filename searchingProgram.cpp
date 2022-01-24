05fa7111b546eac00bb98c01767bd4366005a5a4
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-24 08:44:10 +0700

    Update searchingProgram.cpp

--- searchingProgram.cpp
+++ searchingProgram.cpp
@@ -1,9 +1,9 @@
-//searching itu adalah salah satu teknik dalam pemrograman 
-//untuk mencari data, apakah data tersebut ada di dalam
-//suatu kumpulan data
+/*searching itu adalah salah satu teknik dalam pemrograman 
+untuk mencari data, apakah data tersebut ada di dalam
+suatu kumpulan data */
 
 //dequential search -> membandingkan  data/angka yang di cari dengan seluruh elemen
-//pada suatu data secara satu persatu
+pada suatu data secara satu persatu
 
 #include<iostream>
 using namespace std;
