cdadfdff1839a6cbc8f4215327f41e34901ff4df
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-28 22:48:30 +0700

    Update sensorNama.cpp

--- sensorNama.cpp
+++ sensorNama.cpp
@@ -10,7 +10,7 @@ int main(){
 	
 	for(char x:nama){
 		//pengkondisian huruf yang akan diubah menjadi bintang (*)
-		if( x== 'a'||x=='A'){
+		if( x== 'a'|| x=='A'){
 			cout<<"*";
 		}
 		//pengkondisian di luar if
